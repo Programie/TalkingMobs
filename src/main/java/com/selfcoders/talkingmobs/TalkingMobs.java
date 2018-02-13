ee0542a3481c5651671ab597ebbccae707b69fa8
Michael Wieland <git@selfcoders.com>
2018-02-13 21:47:50 +0100

    Add author and website information to "version" subcommand

--- src/main/java/com/selfcoders/talkingmobs/TalkingMobs.java
+++ src/main/java/com/selfcoders/talkingmobs/TalkingMobs.java
@@ -56,6 +56,8 @@ public final class TalkingMobs extends JavaPlugin {
                 PluginDescriptionFile description = getDescription();
 
                 sender.sendMessage(ChatColor.GOLD + description.getName() + " " + ChatColor.RED + description.getVersion());
+                sender.sendMessage(ChatColor.DARK_GREEN + "by Programie");
+                sender.sendMessage(ChatColor.DARK_GREEN + description.getWebsite());
                 return true;
         }
 
